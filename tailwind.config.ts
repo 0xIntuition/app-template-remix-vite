10bb4ed2bd517a4d521910eb2479cfe22cf72c01
0xjojikun <jojikun@intuition.systems>
2024-02-23 17:35:31 -0700

    fix: temp fix types in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -141,10 +141,10 @@ module.exports = {
   },
   plugins: [
     require('tailwindcss-animate'),
-    function ({ matchUtilities, theme }) {
+    function ({ matchUtilities, theme }: { matchUtilities: any; theme: any }) {
       matchUtilities(
         {
-          'bg-grid': (value) => ({
+          'bg-grid': (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
             )}")`,
@@ -158,7 +158,9 @@ module.exports = {
 
       matchUtilities(
         {
-          highlight: (value) => ({ boxShadow: `inset 0 1px 0 0 ${value}` }),
+          highlight: (value: string) => ({
+            boxShadow: `inset 0 1px 0 0 ${value}`,
+          }),
         },
         {
           values: flattenColorPalette(theme('backgroundColor')),
