e905957c64fbe7b0bb55de1bdb3fe7f4b0d0dfe6
jonathanprozzi <jonathanprozzi@gmail.com>
2024-02-27 00:38:04 -0700

    Updates contract address

--- app/lib/utils/constants.ts
+++ app/lib/utils/constants.ts
@@ -9,7 +9,7 @@ export const DEFAULT_STRATEGY_OPTIONS = {
 }
 
 export const MULTIVAULT_CONTRACT_ADDRESS =
-  '0x5a4E28Aa014237a9997c6E8F597E5f0076083eF3' // TODO: Change this to NEW IMPLEMENTATION
+  '0x51eB0DCf0411714a3E126376a700532E6FC2e7d4'
 
 export const DOCUMENTATION_URL = 'https://docs.intuition.systems/'
 export const EXAMPLES_URL = 'https://docs.intuition.systems/example-apps/'
