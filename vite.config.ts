0f2c473dbdd381fb772bdb0d251fcb76f9ab2293
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-03-01 13:31:33 -0500

    feat: :wrench: Add million compiler plugin to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { installGlobals } from '@remix-run/node'
 import { defineConfig } from 'vite'
 import { flatRoutes } from 'remix-flat-routes'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import million from 'million/compiler'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-
 installGlobals()
 
 export default defineConfig({
   plugins: [
+    million.vite({ auto: true }),
     remix({
       ignoredRouteFiles: ['**/.*'],
       routes: async (defineRoutes) => {
